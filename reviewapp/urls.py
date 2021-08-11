67b599c7365212e4a405d65a81fd6881b31cc8e0
qor-101 <qor6119@gmail.com>
2021-08-12 01:22:40 +0530

    Added Functionality to write reviews

--- reviewapp/urls.py
+++ reviewapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
         path('',views.index,name='default page'),
+        path('write-review',views.new_review,name='new review'),
+        path('upload',views.upload_review,name='upload review'),
+        path('upload-success',views.upload_success,name='upload success'),
+        path('upload-failed',views.upload_failed,name='upload failed'),
 ]
