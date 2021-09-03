a4ff4f8650b28fbedf2b600c3912480986470ec2
qor-101 <qor6119@gmail.com>
2021-09-04 01:03:56 +0530

    Added Functionality: Searching Reviews

--- reviewapp/views.py
+++ reviewapp/views.py
@@ -17,18 +17,27 @@ def new_review(request):
 
 @csrf_exempt
 def upload_review(request):
+    
     db_name = request.POST['branch']
+    
     college_name = request.POST['college']
     college_name = college_name.replace(" ","")
     college_name = college_name.lower()
-    print(college_name)
+    
+    course_name = request.POST['course']
+    course_name = course_name.replace(" ","")
+    course_name = course_name.lower()
+    
     coll_name = college_name
+    
     doc = {
         "college":college_name,
-        "course":request.POST['course'],
+        "course":course_name,
         "review":request.POST['review'],
     }
+    
     flag = func_upload(db_name,coll_name,doc)
+    
     if flag:
         return redirect('/upload-success')
     else:
@@ -41,3 +50,42 @@ def upload_success(request):
 @csrf_exempt
 def upload_failed(request):
     return HttpResponse("Something is not right, Please Try Again")
+
+@csrf_exempt
+def search(request):
+    return render(request,"search.html")
+
+@csrf_exempt
+def fetch(request):
+    
+    college_name = request.POST['college']
+    college_name = college_name.replace(" ","")
+    college_name = college_name.lower()
+    
+    course_name = request.POST['course']
+    course_name = course_name.replace(" ","")
+    course_name = course_name.lower()
+    
+    kei = { 'course' : course_name }
+    
+    returned = func_retreive(request.POST['branch'],college_name,kei)
+    
+    res = []
+    for x in returned:
+        res.append(x)
+
+    if(len(res)==0):
+        return redirect('/noresults')
+
+    global c
+    c = {'items':res}
+    
+    return redirect('/results')
+
+@csrf_exempt
+def results(request):
+    return render(request,"results.html",c,content_type='text/html')
+
+@csrf_exempt
+def noresults(request):
+    return HttpResponse("No Reviews Found :(")
